483c1702bf8db9fb4802d085b0b1a6f9ccdf806e
UshanShakya <usnshakya4@gmail.com>
2020-03-10 09:45:11 +0545

    Added the SQL for storing

--- Online Shopping/OnlineShopping.WebUI/App_Start/UnityConfig.cs
+++ Online Shopping/OnlineShopping.WebUI/App_Start/UnityConfig.cs
@@ -1,6 +1,7 @@
 using OnlineShopping.Core.Contracts;
 using OnlineShopping.Core.Models;
 using OnlineShopping.DataAccess.InMemory;
+using OnlineShopping.DataAccess.SQL;
 using System;
 
 using Unity;
@@ -45,8 +46,8 @@ namespace OnlineShopping.WebUI
 
             // TODO: Register your type's mappings here.
             // container.RegisterType<IProductRepository, ProductRepository>();
-            container.RegisterType<IRepository<Product>, InMemoryRepository<Product>>();
-            container.RegisterType<IRepository<ProductCategory>, InMemoryRepository<ProductCategory>>();
+            container.RegisterType<IRepository<Product>, SQLRepository<Product>>();
+            container.RegisterType<IRepository<ProductCategory>, SQLRepository<ProductCategory>>();
         }
     }
 }

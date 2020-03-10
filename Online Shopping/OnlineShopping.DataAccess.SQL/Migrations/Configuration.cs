3bd500bb7a24725e6016debc78f47272dc167ba0
UshanShakya <usnshakya4@gmail.com>
2020-03-10 09:06:20 +0545

    Added Entity framework to connect to the database

---
+++ Online Shopping/OnlineShopping.DataAccess.SQL/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace OnlineShopping.DataAccess.SQL.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<OnlineShopping.DataAccess.SQL.DataContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(OnlineShopping.DataAccess.SQL.DataContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
